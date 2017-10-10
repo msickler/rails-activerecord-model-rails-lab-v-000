b75e39420e748971f7e55f874a8841166eddf505
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-10 14:47:03 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,9 +1,11 @@
-class CreateStudents < ActiveRecord::Migration 
+class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |s|
       s.string :first_name
       s.string :last_name
+
+      t.timestamps null: false
     end
-      
+
     end
 end
